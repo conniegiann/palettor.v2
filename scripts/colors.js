08a4a18d88f5c147dbceef05685a3db21f0f3cfd
Connie <conniegiann@gmail.com>
2015-07-27 22:47:16 +1000

    Colors

---
+++ scripts/colors.js
@@ -0,0 +1,17 @@
+// $(document).ready(function () {
+
+//    $('.box1').mousemove(function (event) {
+//     console.log(event);
+//        var red = Math.round(((event.pageX - $(this).offset().left) / $(this).width()) * 221);
+//        var green = Math.round(((event.pageY - $(this).offset().top) / $(this).height()) * 221);
+
+//        var blue = Math.round(221 - ((red + green) / 2));
+//        var rgb = 'rgb(' + [red, green, blue].join(',') + ')';
+
+
+//         $('.box1').css('background-color', rgb);
+//         $('.colorDisplay1').text(rgb);
+//    });
+
+
+// });
